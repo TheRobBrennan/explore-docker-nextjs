812d851317cfd0292f55380784fbfaae2679073d
Rob Brennan <rob@therobbrennan.com>
2019-12-14 11:20:26 -0800

    Added index page for our NextJS app

---
+++ examples/01-get-started-with-next-js/front-end/pages/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { NextPage } from 'next';
+
+import { Hello } from '../components/hello';
+
+const IndexPage: NextPage = () => {
+  return <Hello />;
+};
+
+export default IndexPage;
